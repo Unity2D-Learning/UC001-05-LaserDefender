7a86b22be976dcc34ff3dd0764194333eb70d3ae
unknown <hello@xrux.at>
2018-11-17 10:42:58 +0100

    limiting player movement

--- Assets/MyGame/Scripts/Player.cs
+++ Assets/MyGame/Scripts/Player.cs
@@ -6,24 +6,39 @@ public class Player : MonoBehaviour {
     private const string AXISHORIZONTAL = "Horizontal";
     private const string AXISVERTICAL   = "Vertical";
     public float moveSpeed = 10f;
+    public float padding = 1f;
+
+    float xMin,yMin;
+    float xMax,yMax;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start ()
+    {
+        SetUpMoveBounderies();
+    }
+
+    private void SetUpMoveBounderies()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         Move();
 	}
 
     private void Move()
     {
         var deltaX = Input.GetAxis(AXISHORIZONTAL) * Time.deltaTime * moveSpeed; //frame rate independent
-        var newXPos = transform.position.x + deltaX;
-
         var deltaY = Input.GetAxis(AXISVERTICAL) * Time.deltaTime * moveSpeed; //frame rate independent
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin,xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin,yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
        
